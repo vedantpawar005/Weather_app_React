3d1e179ffb48a9849cc7e6002045d8ac1f3942a0
vedantpawar005 <vedantpawar005@gmail.com>
2025-09-17 18:16:57 +0530

    Built just basic UI

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,7 +1,21 @@
+import { useState } from "react";
 function Weatherapp()
 {
+  const [city,getcity]=useState("");
   return(
-    <><h1 class="absolute top-4 left-1/2 -translate-x-1/2 text-2xl font-bold">Hello</h1></>
+    <div class="flex flex-col items-center mt-6">
+  <h1 class="text-3xl font-bold mb-4">Weather App</h1>
+  <input placeholder="Enter City"
+   class="border rounded-lg px-3 py-2 w-64 text-center focus:outline-none focus:ring-2 focus:ring-blue-500">
+
+   </input>
+   <button
+        className="mt-3 bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
+      >
+        Get Weather
+      </button>
+  <p class="mt-2 text-lg text-gray-600">Current Temprature</p>
+</div>
   );
 }
 export default Weatherapp;
